7cfa320f75f9659d031174de71a25c592a687b9a
FozzeY <tehfozzey@gmail.com>
2021-12-22 00:12:46 +0300

    -doubleammo parameter for Strife

--- src/strife/p_inter.c
+++ src/strife/p_inter.c
@@ -83,10 +83,12 @@ boolean P_GiveAmmo(player_t* player, ammotype_t ammo, int num)
         num = clipammo[ammo]/2;
 
     if(gameskill == sk_baby
-        || gameskill == sk_nightmare)
+        || gameskill == sk_nightmare
+        || crispy->moreammo && !demoplayback)
     {
         // give double ammo in trainer mode,
         // you'll need in nightmare
+        // [crispy] double ammo
         num <<= 1;
     }
 
