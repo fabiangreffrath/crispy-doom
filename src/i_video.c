8c42d576432ed7181c71c20de8dbe5eecde93f70
Michael Day <mikeguy42@gmail.com>
2022-03-03 21:23:55 -0500

    Revert "Move fractionaltic calculation"
    This reverts commit 60bb94968f642edb1f8c56fb6a9a9f5f2e2b6502.

--- src/i_video.c
+++ src/i_video.c
@@ -892,6 +892,12 @@ void I_FinishUpdate (void)
 
     SDL_RenderPresent(renderer);
 
+    // [AM] Figure out how far into the current tic we're in as a fixed_t.
+    if (crispy->uncapped)
+    {
+	fractionaltic = (int64_t)I_GetTimeMS() * TICRATE % 1000 * FRACUNIT / 1000;
+    }
+
     // Restore background and undo the disk indicator, if it was drawn.
     V_RestoreDiskBackground();
 }
