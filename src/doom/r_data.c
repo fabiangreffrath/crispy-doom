ec1a645ee4b919abfd60440306a827bae6f03348
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-02-02 21:57:07 +0300

    Add comment(s) for introduced pal_color[] array

--- src/doom/r_data.c
+++ src/doom/r_data.c
@@ -167,7 +167,8 @@ fixed_t*	spritewidth;
 fixed_t*	spriteoffset;
 fixed_t*	spritetopoffset;
 
-lighttable_t	*colormaps, *pal_color;
+lighttable_t	*colormaps;
+lighttable_t	*pal_color; // [crispy] array holding palette colors for true color mode
 
 // [FG] check if the lump can be a Doom patch
 // taken from PrBoom+ prboom2/src/r_patch.c:L350-L390
