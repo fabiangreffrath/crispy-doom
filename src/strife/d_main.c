1a8f10cec23e56a9a58ee62e662ddf351a254f76
Noseey <omega_1986@hotmail.com>
2025-12-13 16:15:09 +0100

    Strife: Adding A11y Extra Lighting, Weapon Flash, palette, Weapon palette.

--- src/strife/d_main.c
+++ src/strife/d_main.c
@@ -512,6 +512,10 @@ void D_BindVariables(void)
     M_BindIntVariable("screensize",             &screenblocks);
     M_BindIntVariable("snd_channels",           &snd_channels);
     M_BindIntVariable("a11y_sector_lighting",   &a11y_sector_lighting); // [crispy]
+    M_BindIntVariable("a11y_extra_lighting",    &a11y_extra_lighting); // [crispy]
+    M_BindIntVariable("a11y_weapon_flash",      &a11y_weapon_flash); // [crispy]
+    M_BindIntVariable("a11y_palette_changes",   &a11y_palette_changes); // [crispy]
+    M_BindIntVariable("a11y_weapon_palette",    &a11y_weapon_palette); // [crispy]
     M_BindIntVariable("vanilla_savegame_limit", &vanilla_savegame_limit);
     M_BindIntVariable("vanilla_demo_limit",     &vanilla_demo_limit);
     M_BindIntVariable("show_endoom",            &show_endoom);
